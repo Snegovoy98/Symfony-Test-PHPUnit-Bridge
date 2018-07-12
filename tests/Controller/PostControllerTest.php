ffdddaf75e398dbdaecae2c68b26bed8d8fd38d7
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-12 16:04:34 +0300

    Write tests for form in post.html.twig

--- tests/Controller/PostControllerTest.php
+++ tests/Controller/PostControllerTest.php
@@ -3,7 +3,6 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Response;
 
 class PostControllerTest extends WebTestCase
 {
@@ -13,14 +12,54 @@ class PostControllerTest extends WebTestCase
 
         $crawler = $client->request('post','/post/hello-world');
 
-        $form = $crawler->selectButton('submit')->form();
-        $form['firstName'] = 'Ivan';
-        $form['lastName'] = 'Ivanov';
-        $form['username'] = 'IvanovMaster';
+        $form = $crawler->selectButton('submit')->form(
+            [
+                'firstName'  => 'Ivan',
+                'lastName' => 'Ivanov',
+                'username' =>'IvanovMaster',
+                'email' => 'testuser@gmail.com',
+                 'address' => 'Kiev',
+                'secondAddress' => 'New York'
+            ]
+        );
 
-        $div = $crawler
+         $crawler
             ->filter('div:contains("Valid first name is required.")')
             ->eq(1)
             ;
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            )
+        );
+
+        $this->assertTrue(
+            $client->getResponse()->isSuccessful(),
+            'response status is 2xx'
+        );
+
+        $client->request(
+          'POST',
+          '/post',
+          [],
+          [],
+          [
+              'Content_TYPE' => 'application/json',
+              'HTTP_REFERER' => '/post'
+          ]
+        );
+
+        $link = $crawler->selectLink('Privacy')->link();
+        $client->click($link);
+        $form = $crawler->selectButton('Continue to checkout')->form();
+        $client->submit($form, ['id' => 'submit']);
+        $newCrawler = $crawler->filter('div.title')
+            ->last()
+            ->parents()
+            ->first()
+        ;
+
+
     }
 }
